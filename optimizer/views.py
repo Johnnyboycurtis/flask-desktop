67cbe1c4ef115a524f55aa185e4f6b1c56c2ddb7
Jonathan Navarrete <johnnyboycurtis@gmail.com>
2018-07-09 11:46:35 -0700

    GA Application is now home

--- optimizer/views.py
+++ optimizer/views.py
@@ -5,15 +5,17 @@ import time
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'ABC123'
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, GAParams
 
 
-@app.route('/', methods = ['GET', 'POST'])
-@app.route('/home')
-def home():
+'''
+Some Demo Views
+'''
+
+@app.route('/demo', methods = ['GET', 'POST'])
+def demo():
     form = RegistrationForm()
-    return render_template('home/home.html', form=form)
-
+    return render_template('demo/demo.html', form=form, title = "Demo")
 
 
 @app.route('/run')
@@ -36,3 +38,22 @@ def generate():
 def progress():
     return Response(generate(), mimetype= 'text/event-stream')
 
+
+'''
+Main App Views
+'''
+
+@app.route("/", methods = ["GET", "POST"])
+@app.route("/home", methods = ["GET", "POST"])
+@app.route("/ga", methods = ["GET", "POST"])
+def ga():
+    form = GAParams()
+    title = "Genetic Algorithm"
+    return render_template('ga/ga.html', form=form, title=title)
+
+
+@app.route("/results", methods = ["POST"])
+def results():
+    generations = request.form['generations']
+    popsize = request.form['popsize']
+    return "<h1>Results</h1><br><br>Generations: {} <br>Population Size: {}".format(generations, popsize)
